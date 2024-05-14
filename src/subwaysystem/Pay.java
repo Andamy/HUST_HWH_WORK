1096f694848e7d7462f48fc451bcd5f912176867
HUST2024java <36759794+Andamy@users.noreply.github.com>
2024-05-14 17:06:33 +0800

    Add files via upload

---
+++ src/subwaysystem/Pay.java
@@ -0,0 +1,14 @@
+
+package subwaysystem;
+
+public abstract class Pay {
+	
+    protected double distance;
+    protected double fee;
+    
+    /**
+     * count the fee
+     */
+    protected abstract double count();
+    
+}
